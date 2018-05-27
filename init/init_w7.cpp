c0486fec833dab12fe03cc90aa09118c113c1e01
MobiusM <danholandi@gmail.com>
2018-05-27 22:58:50 +0300

    w7: init: Also override certain vendor build properties
    Change-Id: I2acce5df07619b4a737994b7d223f77b09425cb5

--- init/init_w7.cpp
+++ init/init_w7.cpp
@@ -77,15 +77,15 @@ void vendor_load_properties() {
     if (serial.substr(0,6) ==  "LGD410") {
         /* D410, D410hn */
         if (check_cmdline("model.name=LG-D410hn")) {
-                property_override("ro.product.device", "w7nds");
-                property_override("ro.product.model", "LG-D410hn");
+                property_override_dual("ro.product.device", "ro.vendor.product.device", "w7nds");
+                property_override_dual("ro.product.model", "ro.vendor.product.model", "LG-D410hn");
                 property_set("ro.nfc.port", "I2C");
         } else {
                 property_override("ro.product.device", "w7ds");
                 property_override("ro.product.model", "LG-D410");
         }
         property_override("ro.build.description", "w7ds_global_com-user 5.0.2 LRX22G.A1423481010 15040202274a7 release-keys");
-        property_override("ro.build.fingerprint", "lge/w7ds_global_com/w7ds:5.0.2/LRX22G.A1423481010/15040202274a7:user/release-keys");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "lge/w7ds_global_com/w7ds:5.0.2/LRX22G.A1423481010/15040202274a7:user/release-keys");
         property_set("persist.multisim.config", "dsds");
         property_set("persist.radio.multisim.config", "dsds");
         property_set("persist.radio.dont_use_dsd", "true");
@@ -93,29 +93,29 @@ void vendor_load_properties() {
     } else if (serial.substr(0,6) ==  "LGD405") {
         /* D405, D405n */
         if (check_cmdline("model.name=LG-D405n")) {
-                property_override("ro.product.model", "LG-D405n");
-                property_override("ro.product.device", "w7n");
+                property_override_dual("ro.product.model", "ro.vendor.product.model", "LG-D405n");
+                property_override_dual("ro.product.device", "ro.vendor.product.device", "w7n");
                 property_set("ro.nfc.port", "I2C");
         } else {
-                property_override("ro.product.model", "LG-D405");
-                property_override("ro.product.device", "w7");
+                property_override_dual("ro.product.model", "ro.vendor.product.model", "LG-D405");
+                property_override_dual("ro.product.device", "ro.vendor.product.device", "w7");
         }
         property_override("ro.build.description", "w7_global_com-user 5.0.2 LRX22G.A1423481010 15040202274a7 release-keys");
-        property_override("ro.build.fingerprint", "lge/w7_global_com/w7ds:5.0.2/LRX22G.A1423481010/15040202274a7:user/release-keys");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "lge/w7_global_com/w7ds:5.0.2/LRX22G.A1423481010/15040202274a7:user/release-keys");
         property_set("persist.radio.multisim.config", "");
         property_set("persist.multisim.config", "");
     } else if (serial.substr(0,6) ==   "LGD415") {
         /* D415 */
-        property_override("ro.product.model", "LG-D415");
-        property_override("ro.product.device", "w7");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "LG-D415");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "w7");
         property_override("ro.build.description", "w7_global_com-user 5.0.2 LRX22G.A1423481010 15040202274a7 release-keys");
-        property_override("ro.build.fingerprint", "lge/w7_global_com/w7ds:5.0.2/LRX22G.A1423481010/15040202274a7:user/release-keys");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "lge/w7_global_com/w7ds:5.0.2/LRX22G.A1423481010/15040202274a7:user/release-keys");
         property_set("persist.radio.multisim.config", "");
         property_set("persist.multisim.config", "");
     } else {
         /* XXX */
-        property_override("ro.product.device", "w7");
-        property_override("ro.product.model", "Please write your model name to agent00791@gmail.com");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "w7");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "Please write your model name to agent00791@gmail.com");
         property_set("persist.radio.multisim.config", "");
         property_set("persist.multisim.config", "");
     }
